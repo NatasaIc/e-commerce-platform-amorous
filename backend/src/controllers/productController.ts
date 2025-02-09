586d5579e7386a8831c0b5d23abe348ba86ed661
NatasaIc <iconomou.ng@gmail>
2025-02-09 09:57:12 +0100

    adding product update and remove in productController

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import Product from '../models/productModel';
 
-//Get all products
+// @desc   GET all products 
+// @route  GET /api/products
+// @access Public
 const getAllProducts = async (req: Request, res: Response) => {
   try {
     const products = await Product.find();
@@ -11,7 +13,9 @@ const getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-//Create new product
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/admin
 const createProduct = async (req: Request, res: Response) => {
   try {
     const newProduct = new Product(req.body);
@@ -22,7 +26,9 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-//Get product by ID
+// @desc    GET product by ID
+// @route   GET /api/products/:id
+// @access  Private/admin
 const getProductById = async (req: Request, res: Response): Promise<void> => {
   try {
     const product = await Product.findById(req.params.id);
@@ -38,11 +44,50 @@ const getProductById = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-//Update product by ID
-const updateProduct = async (req: Request, res: Response) => {};
+// @desc    Update a product
+// @route   PATCH /api/products/:id
+// @access  Private/admin
+const updateProduct = async (req: Request, res: Response): Promise<void> =>  {
+  try {
+    console.log('Received update request for product ID:', req.params.id);
 
-//Delete product by ID
-const deleteProduct = async (req: Request, res: Response) => {};
+    // Start timer
+    const startTime = Date.now();
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    const duration = Date.now() - startTime;
+    console.log(`Product update took ${duration}ms`);
+
+
+    if (!product) {
+      res.status(404).json({ message: 'Product not found '});
+      return;
+    }
+    res.json(product);
+  } catch (error) {
+    console.error('Error updating product:', error);
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+// @desc    Remove a product
+// @route   Delete /api/products/:id
+// @access  Private/admin
+const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+    res.json({ message: 'Product deleted successfully' });
+    
+  } catch (error) {
+    console.error('Error updating product:', error);
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
 
 export const productController = {
   getAllProducts,
